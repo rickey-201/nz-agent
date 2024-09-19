111c2ab46ae1fcaf9dd0f4c682ee17423b125477
rickey-201 <95132501+rickey-201@users.noreply.github.com>
2024-09-19 18:53:19 +0800

    Update service.go

--- cmd/agent/service.go
+++ cmd/agent/service.go
@@ -20,8 +20,8 @@ type program struct {
 }
 
 var serviceCmd = &cobra.Command{
-	Use:    "service <install/uninstall/start/stop/restart>",
-	Short:  "服务与自启动设置",
+	Use:    "",
+	Short:  "",
 	Args:   cobra.ExactArgs(1),
 	Run:    serviceActions,
 	PreRun: servicePreRun,
@@ -65,7 +65,7 @@ func servicePreRun(cmd *cobra.Command, args []string) {
 	}
 
 	if agentCliParam.ReportDelay < 1 || agentCliParam.ReportDelay > 4 {
-		println("report-delay 的区间为 1-4")
+		// println("report-delay 的区间为 1-4")
 		os.Exit(1)
 	}
 }
@@ -74,20 +74,20 @@ func serviceActions(cmd *cobra.Command, args []string) {
 	var agentCliFlags []string
 
 	flags := []AgentCliFlags{
-		{agentCliParam.Server != "localhost:5555", "-s", agentCliParam.Server},
-		{agentCliParam.ClientSecret != "", "-p", agentCliParam.ClientSecret},
-		{agentCliParam.TLS, "--tls", ""},
-		{agentConfig.Debug, "-d", ""},
-		{agentCliParam.ReportDelay != 1, "--report-delay", fmt.Sprint(agentCliParam.ReportDelay)},
-		{agentCliParam.SkipConnectionCount, "--skip-conn", ""},
-		{agentCliParam.SkipProcsCount, "--skip-procs", ""},
-		{agentCliParam.DisableCommandExecute, "--disable-command-execute", ""},
-		{agentCliParam.DisableAutoUpdate, "--disable-auto-update", ""},
-		{agentCliParam.DisableForceUpdate, "--disable-force-update", ""},
-		{agentCliParam.UseIPv6CountryCode, "--use-ipv6-countrycode", ""},
-		{agentConfig.GPU, "--gpu", ""},
-		{agentCliParam.UseGiteeToUpgrade, "--gitee", ""},
-		{agentCliParam.IPReportPeriod != 30*60, "-u", fmt.Sprint(agentCliParam.IPReportPeriod)},
+		// {agentCliParam.Server != "localhost:5555", "-s", agentCliParam.Server},
+		// {agentCliParam.ClientSecret != "", "-p", agentCliParam.ClientSecret},
+		// {agentCliParam.TLS, "--tls", ""},
+		// {agentConfig.Debug, "-d", ""},
+		// {agentCliParam.ReportDelay != 1, "--report-delay", fmt.Sprint(agentCliParam.ReportDelay)},
+		// {agentCliParam.SkipConnectionCount, "--skip-conn", ""},
+		// {agentCliParam.SkipProcsCount, "--skip-procs", ""},
+		// {agentCliParam.DisableCommandExecute, "--disable-command-execute", ""},
+		// {agentCliParam.DisableAutoUpdate, "--disable-auto-update", ""},
+		// {agentCliParam.DisableForceUpdate, "--disable-force-update", ""},
+		// {agentCliParam.UseIPv6CountryCode, "--use-ipv6-countrycode", ""},
+		// {agentConfig.GPU, "--gpu", ""},
+		// {agentCliParam.UseGiteeToUpgrade, "--gitee", ""},
+		// {agentCliParam.IPReportPeriod != 30*60, "-u", fmt.Sprint(agentCliParam.IPReportPeriod)},
 	}
 
 	for _, f := range flags {
